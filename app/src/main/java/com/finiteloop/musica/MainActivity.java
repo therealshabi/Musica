3d59a503b84d471f2ad7ba0113f82299c41f1cf4
shivam_gupta_210397 <gupta.shivam97@gmail.com>
2017-01-21 14:14:35 +0530

    Profile Description and event notification

---
+++ app/src/main/java/com/finiteloop/musica/MainActivity.java
@@ -0,0 +1,44 @@
+package com.finiteloop.musica;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button event;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button_profileDescription);
+        event=(Button)findViewById(R.id.button_event_notification_dialog_box);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),ProfileDescriptionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                View view= getLayoutInflater().inflate(R.layout.event_invitation,null);
+                builder.setView(view);
+                final AlertDialog dialog=builder.create();
+                dialog.show();
+            }
+        });
+    }
+}
