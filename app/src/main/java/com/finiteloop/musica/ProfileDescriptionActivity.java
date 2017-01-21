73b215926397cf33c112b28f3ff88510287385de
shahbaz <shahbaz.h96@gmail.com>
2017-01-22 02:26:19 +0530

    Merged and Updated

--- app/src/main/java/com/finiteloop/musica/ProfileDescriptionActivity.java
+++ app/src/main/java/com/finiteloop/musica/ProfileDescriptionActivity.java
@@ -1,13 +1,27 @@
 package com.finiteloop.musica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileDescriptionActivity extends AppCompatActivity {
+
+    Button mFollowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_description);
+
+        mFollowButton = (Button) findViewById(R.id.activity_profile_description_follow_button);
+
+        mFollowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfileDescriptionActivity.this,MainActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/finiteloop/musica/SignInActivity.java
+++ app/src/main/java/com/finiteloop/musica/SignInActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
     TextView mSignUp;
+    Button mSignIn;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
@@ -19,11 +21,19 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         mSignUp = (TextView) findViewById(R.id.activity_sign_in_sign_up_text);
+        mSignIn = (Button) findViewById(R.id.activity_sign_in_button);
 
         mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            }
+        });
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignInActivity.this, ProfileDescriptionActivity.class));
             }
         });
     }
