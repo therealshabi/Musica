d14dc0392336b9d862da6e9de00732960dda21cd
shivam_gupta_210397 <gupta.shivam97@gmail.com>
2017-02-04 19:36:12 +0530

    HomeStreamActivity Ui changes, Drawer added

--- app/src/main/java/com/finiteloop/musica/HomeStreamActivity.java
+++ app/src/main/java/com/finiteloop/musica/HomeStreamActivity.java
@@ -1,29 +1,97 @@
 package com.finiteloop.musica;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class HomeStreamActivity extends AppCompatActivity {
 
     public RecyclerView recyclerView;
+    private ImageView searchButton;
+    private ImageView profileButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_stream);
 
-        Toast.makeText(this, "Activity Started", Toast.LENGTH_SHORT).show();
+        searchButton=(ImageView)findViewById(R.id.activity_home_stream_searchButton);
+        profileButton=(ImageView)findViewById(R.id.activity_home_stream_profileButton);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.activity_home_stream_drawerLayout);
+        navigationView=(NavigationView)findViewById(R.id.activity_home_stream_navigationView);
 
         recyclerView=(RecyclerView)findViewById(R.id.home_stream_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
         recyclerView.setAdapter(new RecyclerViewAdapter());
 
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Search Activity shows up ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Profile Activity shows up ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_menu_home:{
+                        Toast.makeText(getBaseContext(),"Home Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_explore:{
+                        Toast.makeText(getBaseContext(),"Explore Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_playlist:{
+                        Toast.makeText(getBaseContext(),"Playlist Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_notifications:{
+                        Toast.makeText(getBaseContext(),"Notifications Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_profile:{
+                        Toast.makeText(getBaseContext(),"Profile Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_report_and_feedback:{
+                        Toast.makeText(getBaseContext(),"Report And Feedback Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_settings:{
+                        Toast.makeText(getBaseContext(),"Settings Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    case R.id.navigation_menu_signOut:{
+                        Toast.makeText(getBaseContext(),"SignOut Button Pressed",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder>{
