619e68e2fedf6b1cd5c3da83e2a8ee95256238d2
therealshabi <shahbaz.h96@gmail.com>
2017-04-11 00:27:59 +0530

    Added Log in checking in Splash Screen

--- app/src/main/java/com/finiteloop/musica/SplashScreenActivity.java
+++ app/src/main/java/com/finiteloop/musica/SplashScreenActivity.java
@@ -5,20 +5,27 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 3000;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 finish();
-                startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                if (mAuth.getCurrentUser() == null)
+                    startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                else
+                    startActivity(new Intent(SplashScreenActivity.this, HomeStreamActivity.class));
             }
         }, SPLASH_TIME_OUT);
 
