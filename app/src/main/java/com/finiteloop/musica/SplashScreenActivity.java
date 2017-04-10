0625be12e4dbff121786a3c587e3a3c252fe69db
therealshabi <shahbaz.h96@gmail.com>
2017-04-11 00:16:44 +0530

    Added Splash Screen Activity

---
+++ app/src/main/java/com/finiteloop/musica/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.finiteloop.musica;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
