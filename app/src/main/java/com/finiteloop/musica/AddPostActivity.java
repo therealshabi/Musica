3a8919dbcb622e1ef59db6572a1708442a5ab499
therealshabi <shahbaz.h96@gmail.com>
2017-04-13 23:07:24 +0530

    Modified Add Post Activity

--- app/src/main/java/com/finiteloop/musica/AddPostActivity.java
+++ app/src/main/java/com/finiteloop/musica/AddPostActivity.java
@@ -1,13 +1,22 @@
 package com.finiteloop.musica;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AddPostActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
+    Spinner mGenreSpinner;
+    ArrayList<String> mGenreArrayList = new ArrayList<>();
+    ArrayAdapter<String> mGenreArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,7 @@ public class AddPostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_post);
 
         mToolbar = (Toolbar) findViewById(R.id.activity_add_post_toolbar);
+        mGenreSpinner = (Spinner) findViewById(R.id.activity_add_post_genre_spinner);
         setSupportActionBar(mToolbar);
 
         if (getSupportActionBar() != null) {
@@ -23,6 +33,32 @@ public class AddPostActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("");
             getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp));
         }
+
+        mGenreArrayList.add("Select Genre of the Song");
+        mGenreArrayList.add("Pop");
+        mGenreArrayList.add("Hip-hop");
+        mGenreArrayList.add("Rock");
+        mGenreArrayList.add("Romatic");
+        mGenreArrayList.add("Classic");
+        mGenreArrayList.add("Bollywood");
+        mGenreArrayList.add("Electronic");
+        mGenreArrayList.add("Acoustic");
+        mGenreArrayList.add("Opera");
+
+        mGenreArrayAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_spinner_dropdown_item, mGenreArrayList);
+        mGenreSpinner.setAdapter(mGenreArrayAdapter);
+
+        mGenreSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
